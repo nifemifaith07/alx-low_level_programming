648bceb2d4a7ac79ac3f64805925b977bf62f1aa
nifemifaith07 <nifemifaith07@gmail.com>
2022-07-02 18:39:52 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ *  Return: 1 (error)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
