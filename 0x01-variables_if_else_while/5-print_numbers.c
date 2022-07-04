50d20fe31079f4212d7af488cbf971307de48cba
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-05 00:50:41 +0100

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	printf("\n");
+
+	return (0);
+}
