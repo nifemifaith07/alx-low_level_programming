c9de8a95641b069a41d64e2d0d2264a957fa739d
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-05 05:53:24 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+			continue;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
