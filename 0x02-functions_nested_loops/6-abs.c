a75f1c8348feb2a8de3f9a00a90bf879aeb61252
nifemifaith07 <nifemifaith07@gmail.com>
2022-07-05 13:51:41 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of integers
+ * @c: integer
+ *
+ * Return: absolute value
+ */
+int _abs(int c);
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (0 - c);
+}
