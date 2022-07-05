9c0cdd8ad380a27ca014b3f0c25f58ca4e3f8255
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-05 06:00:56 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * program that prints all possible combinations
+ * of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * he two numbers should be separated by a space
+ * All numbers should be printed with two digits
+ * The combination of numbers must be separated
+ * by comma, followed by a space
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (j = 1; j <= 9; j++)
+		for (j = 1; j <= 99; j++)
 		{
-			for (k = 2; k <= 9; k++)
+			if (j > i)
 			{
-				if (i < j && j < k)
-				{
-					putchar('0' + i);
-					putchar('0' + j);
-					putchar('0' + k);
+				putchar(((i / 10)) + '0');
+				putchar(((i % 10)) + '0');
+				putchar(' ');
+				putchar(((j / 10)) + '0');
+				putchar(((j % 10)) + '0');
 
-					if (!(i == 7 && j == 8 && k == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if (!(i == 98 && j == 99))
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
