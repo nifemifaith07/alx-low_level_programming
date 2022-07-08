913440f9afe854d101da8c6756ae4210991e4fd4
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-08 05:24:26 +0100

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,15 +4,15 @@
  * _isupper - checks for upper case character
  * @c: character to check
  *
- * Return: 1if uppercase, 0 if otherwise
+ * Return: 1 if uppercase, 0 if otherwise
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		Return (1);
+		return (1);
 	}
 	else
-		Return (0);
+		return (0);
 }
