ea1f6521b170d9f763d7ddf1d8e20de250cc7b8d
nifemifaith07 <nifemifaith07@gmail.com>
2022-07-08 06:12:28 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2&4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		if (j == 50 ||  j == 52);
+		{
+			break;
+		}
+		else
+			_putchar(j);
+	}
+	_putchar(10);
+}
