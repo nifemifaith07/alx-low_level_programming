d6328b88a2ceeddfc3e424e7aac1050bf1eb54fc
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-09 00:04:44 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_line - prints a line
+* @n: no of lines
+*
+* Return: nothing
+
+void print_line(int n)
+{
+	int c;
+
+	c = 0;
+	while (c < n)
+	{
+		_putchar(95);
+
+		c++;
+	}
+	_putchar(10);
+}
