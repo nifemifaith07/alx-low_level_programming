31c00f5b2740f05a49a8ac53565fa4a6757cc37c
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-10 11:36:46 +0100

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/××
+ × main - Entry  point
+ ×
+ × Return: Nothing 
+ ×/
+
+int main(void)
+{
+	 int m;
+
+	 for (m = 0; m < 100; m++)
+	 {
+		 if ((m % 3) == 0 && (m % 5) == 0)
+		 {
+			 printf("FizzBuzz ");
+		 }
+		 if(m % 3 == 0)
+		 {
+			 printf("Fizz ");
+		 }
+		 if (m % 5 == 0)
+		 {
+			 printf("Buzz ");
+		 }
+	 }
+	 printf("Buzz");
+	 printf("\n");
+	 returne (0);
+}
