306a1317cf22e5bb6c27d914b89a230a59783617
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-10 04:08:20 +0100

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,14 +12,14 @@ void print_square(int size)
 	int c, d;
 
 	if (size <= 0)
-		_putchar('\n');
+		_putchar(10);
 
 	for (c = 0; c < size; c++)
 	{
 		for (d = 0; d < size; d++)
-			{
-				_putchar(35);
-			}
+		{
+			_putchar(35);
+		}
 		_putchar(10);
 	}
 }
