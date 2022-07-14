2c029ba70389406b6d683bae4e31ff62b77801da
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-14 18:33:04 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - fumction that returns lenght of a string
+ * @s: string
+ * Return: string lenght
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (i = 0; *s[i] != 0; i++)
+		;
+
+	return (i);
+}
