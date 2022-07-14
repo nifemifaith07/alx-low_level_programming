28f886e7c0c1a54f800e9a57e601efdb04d437c3
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-14 19:25:04 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _puts - prints a string followed by newline to stdout
+* @str: string
+* Return: nothing
+*/
+
+void _puts(char *str)
+{
+	int i = o;
+
+	while (str[i] != '/0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
