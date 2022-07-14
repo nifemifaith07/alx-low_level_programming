8b9499e9fed7c5da7e63a0982cd43402760ec3f0
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-14 18:17:54 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,10 @@
 #define _MAIN_H_
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void puts2(char *str); 
+void puts2(char *str);
+
 #endif
