1dd2dc9044d9c8e5484f4f2226045d0bae881166
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-15 19:04:49 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - cincatenates two strings using at most nbytes from src 
+ * @src: string to concatenate
+ * @dest: destination
+ * @n: number of bytes of strings
+ *
+ * Return: pointer to resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
