d5c3a70e25f2bc192fff73be31ad6886edfe1929
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-15 18:09:30 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination
+ * @src: string to concatenate
+ *
+ * Return: resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+
+
+}
