8ac7bd163ab608d497454358791d70409ad651e2
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-15 18:37:50 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j;
 
-
-
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
 }
