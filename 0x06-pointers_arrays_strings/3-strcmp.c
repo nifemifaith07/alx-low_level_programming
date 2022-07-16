8c9dee55ebf3c15127f0cfbe4d48e215354aece5
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-16 10:55:59 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if s1[i] = s2[i]
-continue
-if s1[i] < s2[i]
-return ( s1[i] - s2[i]);
-if s1[i] > s2[i]
-return (s1[i]
-}
+	int i = 0;
 
-
-
-
-
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }
