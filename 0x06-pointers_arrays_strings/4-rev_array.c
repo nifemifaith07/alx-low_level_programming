0282ece3de200346145ea28daa1a18af4d3872eb
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-16 11:19:32 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: The array
+ * @n: number of elements of the array
+ *
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int k, i = 0;
+
+	i < n;
+	while (a[i] != '\0')
+		i++;
+
+	for (k = 0; k < n; k++)
+	{
+		a[k] = a[i];
+		i--;
+	}
+}
