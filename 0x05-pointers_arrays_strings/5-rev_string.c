68ad36b1afa6930057c7b902b3c0a656e20c6efe
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-16 20:55:48 +0100

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses string
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int len, i,tmp;
+
+	for (len = 0; s[len] != '\0'; len++)
+		;
+	len--;
+	i = 0;
+	while (i < len)
+	{
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+		i++;
+		len--;
+	}
+}
