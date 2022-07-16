748172cd60a28c11da8fb5a816a44fd06d31f0f0
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-16 16:12:56 +0100

    Create 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string
+ * Retutn: pointer to the string
+ */
+
+char *leet(char *s);
+{
+	int y, i = 0;
+	char src[] = {'a', 'A', 'E', 'e', 't', 'T', 'l', 'L', 'o', 'O'};
+	char sub[] = {'4', '4', '3', '3', '7', '7', '1', '1', '0', '0'};
+
+	while (s[i])
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (s[i] == src[y])
+			{
+				s[i] == sub[y];
+			}
+		}
+		i++;
+	}
+}
