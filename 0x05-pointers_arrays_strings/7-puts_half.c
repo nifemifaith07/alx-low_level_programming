3df6dc53ec5dbe08802650404b90c62fab67b21c
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-17 15:35:10 +0100

    print second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half the string or half (=(lenght-1) 2)) bytes
+ * of string if string lenght is odd, followed by newline
+ * @str: string parameter
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int mid, len, i;
+
+	len = 0;
+	while (str[len] != '\n')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		mid = len / 2;
+		for (i = mid; i < len; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		mid = (len - 1) / 2;
+		for (i = mid + 1; i < len; i++)
+			_putchar(str[i]);
+	}
+}
