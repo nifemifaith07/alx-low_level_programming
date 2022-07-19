b5940c07daa6569195bfaada9fd4e6683ca09c5b
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-19 02:43:31 +0100

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of a of pointer to char
+ * @s: pointet to a pointer
+ * @to: character
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+  *s = to;
+}
