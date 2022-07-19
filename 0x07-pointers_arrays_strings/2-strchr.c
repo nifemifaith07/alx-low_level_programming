87c5e28294dfeba80537b07bebe85f570d4b0cb6
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-19 01:23:08 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,12 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
-			break;
 		}
 	}
-	return('\0');
+	return (0);
 }
