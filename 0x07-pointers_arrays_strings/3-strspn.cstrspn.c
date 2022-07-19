4f174ec1cbf3c6a078a8a2cb27311120c529ba2c
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-19 02:23:07 +0100

    Create 3-strspn.cstrspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.cstrspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strspn - gets lenght of a prefix subtring
+* @s: string
+* @accept: substring
+*
+* Return: number of bytes in the initial segment of s 
+* which consist only of bytes from accept
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int j;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if *s == accept[i];
+			j++;
+			break;
+		}
+		else if (accept(i + 1) == '\0')
+		{
+			return (j);
+		}
+		s++;
+	}
+	return (j);
+
+}
