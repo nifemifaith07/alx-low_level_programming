7e214e0cfff7d0f8d213b9e4c7a38a76e19ea20c
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-20 03:07:51 +0100

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include ""main.h
+
+/**
+ * print_diagsums - prints sum of diagonal elements
+ * @a: array of integers[2D]
+ * @size: array size and = size *size
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int n, sum1 = 0, sum2 = 0;
+
+	for (n = 0; n < size; n++)
+	{
+		sum1 +=  a[n];
+		sum2 += a[size - n - 1];
+	}
+	printf("%d", sum1);
+	printf("%d", sum2);
+}
