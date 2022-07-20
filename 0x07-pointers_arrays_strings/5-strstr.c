09b59d98b592f212d6b330fc16c99d64be6f6057
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-20 01:31:47 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,14 +15,9 @@ char *_strstr(char *haystack, char *needle)
 	{
 		while (*needle != '\0' && *haystack == *needle)
 		{
-			if (haystack++ == needle++)
-			{
-				haystack++;
-				needle++;
-			}
-			else if ((haystack++ != needle++) && *needle++ == '\0')	
-				needle++;
-		}
+			haystack++;
+			needle++;
+		}	
 		if (*needle == '\0')
 		{
 			return (haystack);
