7f2646416d4fc9597d64abf3690b033da9bf9ef9
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-20 01:28:48 +0100

    Update and rename 5-strstr. to 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -20,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 				haystack++;
 				needle++;
 			}
-			else if ((haystack++ != needle++) && needle++ == '\0')	
+			else if ((haystack++ != needle++) && *needle++ == '\0')	
 				needle++;
 		}
 		if (*needle == '\0')
