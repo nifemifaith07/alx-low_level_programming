675a1920fe2109a321f2bf89fa1bfac9979566ec
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-20 01:44:15 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,6 +7,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+void print_diagsums(int *a, int size);
+void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
+
+
 #endif
