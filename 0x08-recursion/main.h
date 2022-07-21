b0dda95141d06ddbe4f3e662ede8fe9564a75f39
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-21 19:03:46 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,12 @@
 #ifndef _MAIN_h_
 #define _MAIN_H_
 
+int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n); 
 
 #endif
