269e209f6c45341b9ef9e6673261a487761f9017
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-07-22 03:33:31 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,25 +2,6 @@
 int _is_divisible(int n, int div);
 
 /**
- * is_prime_number - returns 1 if the input integer is a prime number
- * otherwise return 0
- * @n: integer
- *
- * Return: 1 or 0
- */
-
-int is_prime_number(int n)
-{
-	div = 2;
-
-	if (n < 2)
-		return (0);
-	if (n >= 2 && n <= 3)
-		return (1);
-	return (_is-divisible(n, div);
-}
-
-/**
  * _is_divisible - check if number is dibisible
  * @n: number
  * @div: divisor
@@ -38,3 +19,22 @@ int is_prime_number(int n)
 		 return (2);
 	 return (_is-divisible(n, div + 1));
  }
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * otherwise return 0
+ * @n: integer
+ *
+ * Return: 1 or 0
+ */
+
+int is_prime_number(int n)
+{
+	div = 2;
+
+	if (n < 2)
+		return (0);
+	if (n >= 2 && n <= 3)
+		return (1);
+	return (_is-divisible(n, div);
+}
