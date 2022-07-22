f44549172a6c9a3b47a11559db4b5dff9c98780f
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-22 19:42:09 +0100

    Update and rename 0-whatsmyname. to 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,8 @@
  * Return: Always 0 (success)
  */
  
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-
-
-
-
+	printf("%s\n", argv[0]);
+	return (0);
 }
