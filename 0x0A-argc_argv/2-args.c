47e02d9f576ad721f91ad25663be39d8f18006e8
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-22 21:53:11 +0100

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,13 +11,11 @@
 
 int main(int argc, char *argv[])
 {
-int i;
+	int i;
 
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
-
-
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
