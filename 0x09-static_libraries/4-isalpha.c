a35b106646eae49d8dbe1984fd942519891e5cc9
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-25 02:21:30 +0100

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for  alphabets
+ * @c: character to check
+ *
+ * Return: 1 if alphabet, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
