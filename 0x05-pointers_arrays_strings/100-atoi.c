f39d7ecffd6271b8a9e08e8bb10bffe0719394ed
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-25 18:01:55 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,2 +1,28 @@
+#include "main.h"
+
+/**
+ * atoi - converts string to integer
+ * @s: string
+ *
+ * Return: the integer(with sign if -ve)
+ */
 int _atoi(char *s)
-{}
+{
+	int result = 0;
+	int sign = 1;
+
+	do {
+
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			result = (result * 10) + (*s - '0');
+
+		else if (result > 0)
+			break;
+
+	} while (*s++);
+
+	return (result * sign);
+}
