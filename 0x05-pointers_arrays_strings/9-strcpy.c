1e9560e663e97867d2a93b3f4a4395988fdbf201
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-25 04:41:41 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
  *
  * Return: pointer to dest
  */
- 
+
 char *_strcpy(char *dest, char *src)
 {
 	int i, len = 0;
 
 	while (src[len] != '\0')
 		len++;
-	for (i = 0; i < len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		dest[i] = src[i];
 	}
