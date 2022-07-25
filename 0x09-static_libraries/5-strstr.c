01aaa663f3045dbea5079d15a32d6b99cd212774
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-25 02:38:40 +0100

    Create 5-strstr.c

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: substring
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *need = needle;
+		char *hay = haystack;
+
+		while (*need != '\0' && *hay == *need)
+		{
+			hay++;
+			need++;
+		}
+
+		if (*need == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
