c77e3aa3ba8b4cefb5fca9a34af343f6b80293a4
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-27 03:02:22 +0100

    Update 9-strcpy.c

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -6,5 +6,18 @@
  * @dest: destination pointer
  * @src: string pointer
  *
- * Return: pointer to dest
-*/
+ * Return: pointer to des
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i <= len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
