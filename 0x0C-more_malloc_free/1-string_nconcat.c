852dc4862b8b44d8620a2c67b20541b2dc4d3806
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-07-30 17:52:40 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,27 +14,26 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s;
-int a1, a2, i, j;
+	char *s;
+	int a1, a2, i, j;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-
-for (a1 = 0; s1[a1] != '\0'; a1++)
-;
-for (a2 = 0; s2[a2] != '\0'; a2++)
-;
-if (n >= a2)
-n = a2;
-s = malloc(sizeof(char * (a1 + n + 1);
-if (s == NULL)
-return (NULL)
-for (i = 0; s1[i] != '\0'; i++)
-s[i] = s1[i];
-for (j = a1; j < a1 + n; j++)
-s[j] = s2[j - a1];
-s[j] = '\0';
-return (s);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (a1 = 0; s1[a1] != '\0'; a1++)
+		;
+	for (a2 = 0; s2[a2] != '\0'; a2++)
+		;
+	if (n >= a2)
+		n = a2;
+	s = malloc(sizeof(char * (a1 + n + 1);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		s[i] = s1[i];
+	for (j = a1; j < a1 + n; j++)
+		s[j] = s2[j - a1];
+	s[j] = '\0';
+	return (s);
 }
