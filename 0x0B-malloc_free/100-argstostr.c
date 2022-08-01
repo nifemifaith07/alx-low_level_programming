9b5775cf0f33627b9697de6e2642b0eacbf9c161
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-01 03:07:45 +0100

    Create 100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * argstostr - concatenates all the arguments of your program
+ * each separated by \n
+ * @ac: argument count
+ * @av: pointer to array of arguments(strings)
+ *
+ * Return: pointer to new string, or NULL if it fails
+
+char *argstostr(int ac, char **av)
+{
+int i, j, k = 0, len =0;
+char *s;
+
+if (ac == 0 || av == NULL)
+return (NULL);
+for (i = 0; i < ac; 1++)
+{
+for (j = 0; av[i][j]; j++)
+len++;
+}
+/* add number of \n needed to len */
+len += ac;
+s = malloc(sizeof(char) * (len + 1);
+if (s == NULL)
+return (NULL);
+for (i = 0; i < ac; i++)
+for (j =0; av[i][j]; j++)
+s[k] = av[i][j];
+k++
+
+}
