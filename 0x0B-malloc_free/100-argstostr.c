c6c378fad74142a26932bc8ce158d8983022a41c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-01 03:18:55 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,27 +7,34 @@
  * @av: pointer to array of arguments(strings)
  *
  * Return: pointer to new string, or NULL if it fails
+ */
 
 char *argstostr(int ac, char **av)
 {
-int i, j, k = 0, len =0;
-char *s;
+	int i, j, k = 0, len =0;
+	char *s;
 
-if (ac == 0 || av == NULL)
-return (NULL);
-for (i = 0; i < ac; 1++)
-{
-for (j = 0; av[i][j]; j++)
-len++;
-}
-/* add number of \n needed to len */
-len += ac;
-s = malloc(sizeof(char) * (len + 1);
-if (s == NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
-for (j =0; av[i][j]; j++)
-s[k] = av[i][j];
-k++
-
-}
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; 1++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+	}
+	/* add number of \n needed to len */
+	len += ac;
+	s = malloc(sizeof(char) * (len + 1);
+	if (s == NULL)
+		   return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j =0; av[i][j]; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		if (s[k] == '\0')
+		{
+			s[k++] = '\n';
+		}
+	}
