1f359e7ba2fe8cc98e10601af29a6af0628ee3b8
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-01 03:28:53 +0100

    concatenates argument

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,7 +11,7 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, len =0;
+	int i, j, k = 0, len = 0;
 	char *s;
 
 	if (ac == 0 || av == NULL)
@@ -27,10 +27,10 @@ char *argstostr(int ac, char **av)
 	len += ac;
 	s = malloc(sizeof(char) * (len + 1));
 	if (s == NULL)
-		   return (NULL);
+		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j =0; av[i][j]; j++)
+		for (j = 0; av[i][j]; j++)
 		{
 			s[k] = av[i][j];
 			k++;
