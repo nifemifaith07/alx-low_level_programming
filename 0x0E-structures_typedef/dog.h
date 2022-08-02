ca50183e27253c2a482716ccc2ca68263e72ba7e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-02 04:02:46 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,12 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog with the following elements
+ * name, type = char *::age, type = float::owner, type = char *
+ */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 
 #endif
