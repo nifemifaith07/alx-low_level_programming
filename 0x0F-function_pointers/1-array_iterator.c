29c307044d01ac75a0713126b2ad733e8b195fa3
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-10 13:53:29 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
  #include "function_pointers.h"
- 
+
  /**
   * array_iterator - executes a function given as a parameter on an array
   * @array: array of int
@@ -8,7 +8,7 @@
   *
   * Return: nothing
   */
- 
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t j;
