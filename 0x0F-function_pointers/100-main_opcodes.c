5f4814e2d4e490fdde6569b97903ad3a4266a559
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-10 19:53:33 +0100

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that prints the opcodes of its own main function.
+ *	Usage: ./main number_of_byte
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int nbytes, index;
+	unsigned char opcodes;
+	int (*add)(int, char **) = main;
+
+	if (args != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < nbytes; index++)
+	{
+		opcodes = *(unsigned char *)add;
+		printf("%.2x", opcodes);
+
+		if (index == nbytes - 1)
+			continue;
+		printf(" ");
+
+		add++;
+	}
+	printf("\n");
+
+	return (0);
+}
