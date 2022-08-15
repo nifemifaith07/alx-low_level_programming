2eb4477905203e71755549266af3d43553f1d107
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-15 13:25:41 +0100

    print 'em

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to firts node
+ *
+ * Return: no of nodes printed
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	/* return 0 if h is null */
+	if (h == NULL)
+		return (0);
+
+	while h->next != NULL
+	{
+		printf("%d\n", h->n);
+		/* go to next node */
+		h = h->next;
+		/* count increment */
+		i++;
+	}
+	/* print last node */
+	printf("%d\n", h->n);
+
+	return (i);
+}
