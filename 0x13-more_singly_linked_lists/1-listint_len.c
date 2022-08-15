612501f8565d859fc37b0b5c9066ff788642722b
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-15 16:56:49 +0100

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,30 @@
-#include  "lists.h "
+#include "lists.h "
 
 /**
+ * listint_len - returns number of elements in listint list
+ * @h: pointer to firts node
  *
+ * Return: no of elements
  */
 
 size_t listint_len(const listint_t *h)
 {
-size_t count;
+	size_t count = 1;
 
+	/* if h is null return 0 */
+	if (h == NULL)
+		return (0);
 
+	/* iterate through the list until next is null */
+	while (h->next != NULL)
+	{
+		/* increase count if value is not null */
+		if (h->n != '\0')
+			count++;
+
+		/* go to next */
+		h = h->next;
+	}
+
+	return (count);
+}
