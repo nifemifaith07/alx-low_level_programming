bb98a4b4bc168264f4e01dcbd835ce6aa819c399
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 14:20:10 +0100

    🆓 me

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 
 /**
+ * free_listint - frees listint_t list
+ * @head: pointer to first node
  *
- *
+ * Return: nothing
  */
 
-void free_listint2(listint_t **head)
+void free_listint(listint_t *head);
 {
+	listint_t *current;
 
-
-
-
-
-
-
-
-
-
-
-
+	/* iterate through each node till head is null */
+	while (head != NULL)
+	{
+		current = head;
+		/* set head to next node */
+		head = head->next;
+		free(current);
+	}
 }
