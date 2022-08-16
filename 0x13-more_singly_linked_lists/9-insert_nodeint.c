f3c4763f3461e07d68921335c159bcb304fb35c0
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 21:05:41 +0100

    🐞🪲🐛

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		/* set newnode as first node */
-		newnode->next =*head
+		newnode->next =*head;
 		*head = newnode;
 	}
 	else if (current->next)
