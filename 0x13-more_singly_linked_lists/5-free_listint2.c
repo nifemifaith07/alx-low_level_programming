37ff167d12b685042adfa9e2d022c0c9f3ddc4b2
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 15:06:22 +0100

    freed

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees listint_t list and sets head to null
+ * @head: pointer to head pointer
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *current;
+
+	if (*head != NULL)
+	{
+		/* set head address to current node */
+		current = *head;
+
+		while (current != NULL)
+		{
+			/* set current to temp */
+			temp = current;
+			/* set nextnode as new current */
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
