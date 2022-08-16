c4e2fe0de748b1c73860df8082c087599bb3542d
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 11:55:13 +0100

    header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
