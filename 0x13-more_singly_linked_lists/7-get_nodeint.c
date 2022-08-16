7ba2d6bc51b23ceae558ddca3be14a3577b3838c
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 17:51:19 +0100

    get node☺️☺️

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - get nth node of listint_t list
+ * @head: pointer to the first node of the list
+ * @index: index of node to access
+ *
+ * Return: nth node of list or NULL if node doesn't exist
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+/* iterate till node before nth node while head is not null */
+	for (i = 0; i < index && head != NULL; i++)
+		head = head->next;
+
+	return (head);
+}
