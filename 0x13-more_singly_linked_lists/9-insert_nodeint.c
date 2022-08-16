52a7da9151dcc6ac349ff5f81de355c218d83972
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-16 21:03:51 +0100

    ¿🐞🪲🐛🐞¿

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
-	return (NULL);
+		return (NULL);
 
 	newnode->n = n;
 	
