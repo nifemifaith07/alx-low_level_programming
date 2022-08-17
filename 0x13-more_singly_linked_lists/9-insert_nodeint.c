9d70a2dc7a3cef148dc82825bb1dac30a0197bda
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-17 01:54:54 +0100

    bugs 🐞🪲🐛🐞

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		i++;
 	}
 
-	if (i < idx -1)
+	if (i < idx - 1)
 		return (NULL);
 
 	newnode->next = current->next;
