7cbef258cea82c9bbc5dbdb7d3468e1786938eb2
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-19 08:03:13 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,16 +1,41 @@
 #include "main.h"
 
+/**
+ * _Atoi - converts chars to binary integers
+ * @str: pointer to string containing the chars
+ *
+ * Return: equivalent binary integer or
+ * 	0 if non binary digit is found
+ */
+ 
 unsigned int _Atoi(const char *str)
 {
 	unsigned int res = 0;
 	int i = 0;
+	unsigned int num;
 
 	for (; str[i] != '\0'; ++i)
-
 		res = res * 10 + str[i] - '0';
+	/* test if res contains only binary digits */
+	num = res;
+	while (num)
+	{
+		if (num % 10 > 1) /* if non binary found return 0 */
+			return (0);
+
+		num = num / 10;
+	}
 
 	return (res);
 }
+
+/**
+ * binary_to_uint - converts binary chars to unsigned decimal
+ * @b: pointer to binary chars
+ *
+ * Return: 0 if b is Null or contains non binary digits
+ * 	or number converted 
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
@@ -23,19 +48,15 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	num = _Atoi(b);
-
+	if (num = 0)
+		return (0):
 	while (num)
 	{
 		last = num % 10;
-		if (last == 0 || 1)
-		{
-			num = num / 10;
+		num = num / 10;
 
-			dec += last * base;
-			base = base * 2;
-		}
-		else
-			return (0);
+		dec += last * base;
+		base = base * 2;
 	}
 
 	return (dec);
