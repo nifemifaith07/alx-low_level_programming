3d2dbe9b1e38f78aec12bb34e4c8bfc5bcf447d9
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-20 02:43:42 +0100

    Create 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+/**
+ * flip_bits - a function that returns the number of bits you
+ *             would need to flip to get from one number to
+ *             another
+ *
+ * @n: decimal number
+ * @m: the other decimal number
+ *
+ * Return: number of bits to flip to get @m from @n
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int diff_bits = 0;
+	unsigned long int diff;
+
+	/*xor n and m*/
+	diff = n ^ m;
+
+	do {
+
+		diff_bits += (diff & 1);
+		diff >>= 1;		
+
+	} while (diff > 0);
+
+	return (diff_bits);
+}
