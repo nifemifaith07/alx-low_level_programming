66c4ee2c18b265a197a5afd323979e99ac38a2fe
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-23 05:03:41 +0100

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on success OR -1 on faliure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_d, w_count, len = 0;
+
+	if (filename == NULL) /* check if file is present */
+		return (-1);
+	/**
+	* open file with create, write only, truncate flags
+	* with read and write access for user only
+	*/
+	file_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR, S_IWUSR)
+	if (file_d == -1) /* check if opening and creation failed */
+		return (-1);
+
+	if (text_content) /* check content !NULL */
+	{
+		while (text_content[len] != '\0')
+			len++;
+
+		w_count = write(file_d, text_content, len);
+		if (w_count == -1)
+			return (-1);
+	}
+
+	close(file_d);
+
+	return (1);
+}
