809804e9ab70a1357e3cd702fdbdfcca503de67e
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-23 04:29:07 +0100

    read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads textfile, prints it to POSIX std output.
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails, number of letters read and printed
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t r_count, w_count;
+	int file_d;
+	char *buf;
+
+	/* check if file is present */
+	if (filename == NULL)
+		return (0);
+
+	file_d = open(filename, O_RDONLY); /* open file */
+	if (file_d == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf = NULL)
+	{
+		free(buf);
+		return (0);
+	}
+
+	r_count = read(file_d, buf, letters); /* read file */
+	if (r_read == -1)
+		return (0);
+
+	w_count = write(STDOUT_FILENO, buf, r_read); /* write file */
+	if (w_count == -1 || r_count != w_count) /*check if write failed*/
+		return (0);
+
+	free(buf);
+
+	close(file_d); /*close file*/
+
+	return (w_count);
+}
