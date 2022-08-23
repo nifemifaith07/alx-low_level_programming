40f7373cc74c964e57231dfb6586a7f28fe33f2d
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-23 05:24:16 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content != '\0')
+		while (text_content[len] != '\0')
 			len++;
 
 		a_count = write(file_d, text_content, len);
