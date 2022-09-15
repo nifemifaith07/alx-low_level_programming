92c1d2a5eee51abe64e6bdbd939a3aa811402540
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-16 00:15:40 +0100

    betty😒🤨😫

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,13 +7,14 @@
  * @r: buffer where the result will be stored
  * @size_r: max size of the buffer
  * Return: a pointer to the result or
- * 	0 if the result can't be stored in r
+ *	0 if the result can't be stored in r
+ * << means leftshift or move to the left
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i = 0, j = 0, len = 0, k, a, b, abs = 0;
-	/* calc lenght of d numbers, save len as higher value */
+
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
@@ -22,36 +23,30 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		len = i;
 	else
 		len = j;
-	/* check len !> size, +1 cos len was counted from 0 */
-	if ((len + 1) > size_r)
+	if ((len + 1) > size_r) /* +1 cos len was counted from 0 */
 		return (0);
 	r[len] = '\0';
-	/* save integer values of n1, n2 in a, b, calc their sum */
 	for (k = len - 1; k >= 0; k--)
 	{
 		i--;
 		j--;
 		if (i >= 0)
-			a = n1[i] - '0';
+			a = n1[i] - '0'; /* store int of n1 in a */
 		else
 			a = 0;
 		if (j >= 0)
-			b = n2[j] - '0';
+			b = n2[j] - '0'; /* int of n2 in b */
 		else
 			b = 0;
-	/* abs is d remainder to take over to next place value */
 		r[k] = (a + b + abs) % 10 + '0';
-		abs = (a + b + abs) / 10;
+		abs = (a + b + abs) / 10; /* remainder to << */
 	}
-	/* extra value to store */
 	if (abs == 1)
 	{
-		/* increase len & check that len !> size */
-		r[len + 1] = '\0';
+		r[len + 1] = '\0'; /* increase len */
 		if ((len + 2) > size_r)
 			return (0);
-		/* move values backwards by 1 */
-		while (len-- >= 0)
+		while (len-- >= 0) /* move vaues backwards by 1 */
 			r[len + 1] = r[len];
 		r[0] = abs + '0';
 	}
