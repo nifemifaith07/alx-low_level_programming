72c271327b479c7ab5ddb73c11f8f506d59aeb10
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-17 09:05:16 +0100

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -117,45 +117,34 @@ int main(int argc, char *argv[])
 
 	if (argc != 3 || check_for_digits(argv))
 	{
-
 		for (ti = 0; e[ti]; ti++)
 			_putchar(e[ti]);
 		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
 		;
-
 	for (l2 = 0; argv[2][l2]; l2++)
 		;
-
 	ln = l1 + l2 + 1;
-
 	a = malloc(ln * sizeof(char));
-
 	if (a == NULL)
 	{
 		for (ti = 0; e[ti]; ti++)
 			_putchar(e[ti]);
 		exit(98);
 	}
-
 	init(a, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
 		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-
 		if (t == NULL)
 		{
 			for (ti = 0; e[ti]; ti++)
 				_putchar(e[ti]);
-
 			free(a);
 			exit(98);
 		}
-
 	}
-
 	_print(a, ln - 1);
-
 	return (0);
 } 
