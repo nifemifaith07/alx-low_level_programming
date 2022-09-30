4688b629bb38165ce36a6bdf801b89b3a1e0fe25
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-30 13:06:51 +0100

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t new = malloc(sizeof(dlistint_t));
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
