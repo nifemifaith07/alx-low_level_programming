7fdbe5cfa69ed376b85a26b1aa838c7f16580290
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-30 22:59:30 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i = 0;
 	dlistint_t *curr = *h, *prev = *h, *new = NULL;
 
-	if (idx == 0 && *h == NULL)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
 	if (*h == NULL || (curr == NULL && idx > 0))
 		return (NULL);
