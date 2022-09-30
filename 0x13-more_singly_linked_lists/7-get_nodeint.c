c856d61a493f00c37b8fe5b01f8c4d11a2929f13
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-30 15:49:44 +0100

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index && head != NULL; i++)
 		head = head->next;
 
-	if (!head)
-		return (NULL);
 	return (head);
 }
