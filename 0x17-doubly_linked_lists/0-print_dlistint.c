f4797fcafbc55354c907fde1867622c187ce5282
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-09-30 10:40:05 +0100

    print elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,21 @@
-size_t print_dlistint(const dlistint_t *h);
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements in a dlistint list
+ * @h: pointr to first node
+ *
+ * Return: no of nodes in dlistint list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n_count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		n_count++;
+	}
+	return (n_count)
+}
